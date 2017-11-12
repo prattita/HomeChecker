ae5034c05149274bcac534204b2e220df5ada920
Gabriel Krell <gabrielkrell@users.noreply.github.com>
2017-11-11 23:08:31 -0600

    Arduino light: Print light level
    every 1s

--- arduino/lightsensor/lightsensor.ino
+++ arduino/lightsensor/lightsensor.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <CurieBLE.h>
 
+const int LIGHT_SENSOR_PIN = A0;
+const String NAME = "demoPi";
+
+void setup() {
+  pinMode(LIGHT_SENSOR_PIN, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.print("Sensor: ");
+  Serial.print(NAME);
+  Serial.print(" Light level: ");
+  Serial.println(getAvgLightLevel(1000));
+//  Serial.println(millis());
 }
+
+int getAvgLightLevel(int duration) {
+  unsigned long int sum;
+  int del = duration / 30;
+  for (int i=0; i<30; i++) {
+    sum += analogRead(LIGHT_SENSOR_PIN);
+    delay(del);
+  }
+  return sum / 30;
+}
+
